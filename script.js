e6e3492680065edb7fd86aee30b9c342bf6501fa
aveencoder <aveen598@gmail.com>
2025-09-20 17:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
        const sel = document.getElementById('colorSelect');
 	   const btn = document.getElementById('btn');
 	
         btn.addEventListener('click',()=>{
 			 if(sel.selectedIndex != -1 ){
 				 sel.remove(sel.selectedIndex);
-			 }
-			 
-		})   
+			}
+        }) 
+	});
